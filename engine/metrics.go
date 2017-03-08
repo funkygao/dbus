496ea72d4cd760a52d4874572e9bb9e0c1ba6980
Funky Gao <funky.gao@gmail.com>
2017-03-08 16:24:50 +0800

    router metrics done

--- engine/metrics.go
+++ engine/metrics.go
@@ -1,19 +1,36 @@
 package engine
 
 import (
-	//"github.com/funkygao/gafka/telemetry"
+	"sync"
+
 	"github.com/funkygao/go-metrics"
 )
 
 type routerMetrics struct {
-	in  map[string]metrics.Meter
-	out map[string]metrics.Meter
+	l sync.RWMutex
+
+	m map[string]metrics.Meter   // key is Packet.Ident
+	c map[string]metrics.Counter // key is Packet.Ident
 }
 
 func newMetrics() *routerMetrics {
-	return &routerMetrics{}
+	return &routerMetrics{
+		m: make(map[string]metrics.Meter, 10),
+		c: make(map[string]metrics.Counter, 10),
+	}
 }
 
-func (m *routerMetrics) update(pack *Packet) {
+func (m *routerMetrics) Update(pack *Packet) {
+	m.l.RLock()
+	_, present := m.m[pack.Ident]
+	m.l.RUnlock()
+	if !present {
+		m.l.Lock()
+		m.m[pack.Ident] = metrics.NewRegisteredMeter(pack.Ident, metrics.DefaultRegistry)
+		m.c[pack.Ident] = metrics.NewRegisteredCounter(pack.Ident, metrics.DefaultRegistry)
+		m.l.Unlock()
+	}
 
+	m.m[pack.Ident].Mark(1)
+	m.c[pack.Ident].Inc(1)
 }
