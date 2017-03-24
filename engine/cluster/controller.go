12417a1d3bd9202c5d879919d3215a49a2ee136e
Funky Gao <funky.gao@gmail.com>
2017-03-24 17:01:38 +0800

    add pkg doc

--- engine/cluster/controller.go
+++ engine/cluster/controller.go
@@ -1,3 +1,6 @@
+// Package cluster provides helix-alike leader/standby model.
+//
+// It rebalances resources to participants and watch for cluster changes.
 package cluster
 
 // Controller is the contral brain of dbus cluster, which assigns resource tickets
@@ -5,7 +8,7 @@ package cluster
 //
 // Controller works by assigning resources to participants according to the registry.
 //
-// For dbus cluster to work, all dbus instances must share the same configurations,
+// For dbus cluster to work, all dbus instances must share the same configuration,
 // using zookeeper as configuration repo is highly suggetsted.
 type Controller interface {
 
