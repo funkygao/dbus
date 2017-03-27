c1e6be1ed40ed34cd06d21f9ca431396e4ee71b7
Funky Gao <funky.gao@gmail.com>
2017-03-27 09:22:57 +0800

    default is cluster enabled

--- cmd/dbusd/option.go
+++ cmd/dbusd/option.go
@@ -54,7 +54,7 @@ func parseFlags() {
 	flag.BoolVar(&options.routerTrack, "routerstat", true, "track router metrics")
 	flag.IntVar(&options.inputPoolSize, "ipool", iPool, "input recycle pool size")
 	flag.IntVar(&options.filterPoolSize, "fpool", fPool, "filter recycle pool size")
-	flag.BoolVar(&options.clusterEnable, "cluster", false, "enable cluster feature")
+	flag.BoolVar(&options.clusterEnable, "cluster", true, "enable cluster feature")
 	flag.IntVar(&options.hubPoolSize, "hpool", hPool, "hub pool size")
 	flag.IntVar(&options.pluginPoolSize, "ppool", pPool, "plugin pool size")
 	flag.StringVar(&options.visualizeFile, "dump", "", "visualize the pipleline to a png file. graphviz must be installed")
