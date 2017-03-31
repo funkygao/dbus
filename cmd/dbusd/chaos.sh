26ec8adf82019b0a805251358055cf5d4179b5d6
Funky Gao <funky.gao@gmail.com>
2017-03-31 17:27:12 +0800

    add a chaos monkey to test cluster feature stability

---
+++ cmd/dbusd/chaos.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#==========================================================
+# bring chaos to dbusd cluster to test controller stability
+#==========================================================
+#
+#------------
+# how to use?
+#------------
+# open 2 terminals:
+# term1$ make c2
+# term2$ sh chaos
+
+for i in `seq 1000`; do
+    ./dbusd -cluster -pprof :10120 -rpc 9877 -api 9897 &
+    pid=$!
+    sleep 10
+    sleep $[ ( $RANDOM % 10 ) + 10 ]s
+    echo "killing $pid"
+    kill $pid
+    sleep $[ ( $RANDOM % 10 ) + 15 ]s
+done
+
