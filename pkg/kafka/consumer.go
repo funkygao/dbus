acb62019427311166d3fac0cac427f4cc0bed6f1
Funky Gao <funky.gao@gmail.com>
2017-04-07 15:14:41 +0800

    wip: kafka consumer consumes multiple targets

--- pkg/kafka/consumer.go
+++ pkg/kafka/consumer.go
@@ -1,48 +1,141 @@
 package kafka
 
 import (
+	"sync"
+
 	"github.com/Shopify/sarama"
 	"github.com/funkygao/dbus/engine"
 )
 
-// Consumer is a kafka low level consumer.
+// Consumer is a kafka low level consumer that can consumer multiple zone/cluster kafka clusters.
 type Consumer struct {
-	c sarama.Consumer
-	sarama.PartitionConsumer
+	dsns    []string
+	cf      *Config
+	stopper chan struct{}
 
-	topic       string
-	partitionID int32
+	consumersMu sync.Mutex
+	consumers   map[zoneCluster]sarama.Consumer
+
+	wg   sync.WaitGroup
+	once sync.Once
+
+	errors   chan error
+	messages chan *sarama.ConsumerMessage
 }
 
-func NewConsumer(dsn string, cf *Config) (*Consumer, error) {
-	zone, cluster, topic, partitionID, err := ParseDSN(dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	c, err := sarama.NewConsumer(engine.Globals().GetOrRegisterZkzone(zone).NewCluster(cluster).BrokerList(), cf.Sarama)
-	if err != nil {
-		return nil, err
-	}
-
+// NewConsumer returns a kafka consumer.
+func NewConsumer(dsns []string, cf *Config) *Consumer {
 	return &Consumer{
-		c:           c,
-		topic:       topic,
-		partitionID: partitionID,
-	}, nil
+		dsns:      dsns,
+		cf:        cf,
+		messages:  make(chan *sarama.ConsumerMessage, cf.consumeChanBufSize),
+		errors:    make(chan error, cf.consumeChanBufSize),
+		stopper:   make(chan struct{}),
+		consumers: make(map[zoneCluster]sarama.Consumer),
+	}
+}
+
+func (c *Consumer) Messages() <-chan *sarama.ConsumerMessage {
+	return c.messages
+}
+
+func (c *Consumer) Errors() <-chan error {
+	return c.errors
 }
 
 func (c *Consumer) Start() error {
-	p, err := c.c.ConsumePartition(c.topic, c.partitionID, sarama.OffsetNewest) // TODO offset
+	tps, err := parseDSNs(c.dsns)
 	if err != nil {
 		return err
 	}
 
-	c.PartitionConsumer = p
+	for _, tp := range tps {
+		consumer, err := sarama.NewConsumer(engine.Globals().GetOrRegisterZkzone(tp.zc.zone).
+			NewCluster(tp.zc.cluster).BrokerList(), c.cf.Sarama)
+		if err != nil {
+			return err
+		}
+		c.consumersMu.Lock()
+		c.consumers[tp.zc] = consumer
+		c.consumersMu.Unlock()
+
+		c.wg.Add(1)
+		go c.consumerCluster(tp)
+	}
+
 	return nil
 }
 
 func (c *Consumer) Stop() error {
-	c.PartitionConsumer.Close()
-	return c.c.Close()
+	err := ErrAlreadyClosed
+	c.once.Do(func() {
+		close(c.stopper)
+
+		for _, c := range c.consumers {
+			if e := c.Close(); e != nil {
+				err = e
+			}
+		}
+
+		c.wg.Wait()
+	})
+
+	return err
+
+}
+
+func (c *Consumer) consumerCluster(tp topicPartitions) {
+	defer c.wg.Done()
+
+	c.consumersMu.Lock()
+	consumer := c.consumers[tp.zc]
+	c.consumersMu.Unlock()
+
+	var wg sync.WaitGroup
+	for topic, partitions := range tp.tps {
+		for _, partitionID := range partitions {
+			// FIXME initial offset
+		RETRY:
+			pc, err := consumer.ConsumePartition(topic, partitionID, sarama.OffsetNewest)
+			if err != nil {
+				if err == sarama.ErrOffsetOutOfRange {
+					// TODO reset offset and retry
+					goto RETRY
+				}
+
+				c.errors <- err
+				return
+			}
+
+			wg.Add(1)
+			go c.consumePartition(pc, &wg)
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *Consumer) consumePartition(pc sarama.PartitionConsumer, wg *sync.WaitGroup) {
+	defer func() {
+		pc.Close()
+		wg.Done()
+	}()
+
+	for {
+		select {
+		case <-c.stopper:
+			return
+
+		case err := <-pc.Errors():
+			c.errors <- err
+
+		case msg, ok := <-pc.Messages():
+			if !ok {
+				c.errors <- ErrConsumerBroken
+				return
+			}
+
+			c.messages <- msg
+		}
+	}
 }
--- pkg/kafka/errors.go
+++ pkg/kafka/errors.go
@@ -5,9 +5,11 @@ import (
 )
 
 var (
-	ErrNotReady   = errors.New("not ready")
-	ErrNotAllowed = errors.New("not allowed")
-	ErrStopping   = errors.New("stopping")
+	ErrNotReady       = errors.New("not ready")
+	ErrNotAllowed     = errors.New("not allowed")
+	ErrStopping       = errors.New("stopping")
+	ErrAlreadyClosed  = errors.New("consumer already closed")
+	ErrConsumerBroken = errors.New("consumer conn broken")
 )
 
 var InvalidPartitionID = int32(-1)
