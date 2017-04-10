f015b76b274179321d2e5a7a00c7712b8d6c63c3
Funky Gao <funky.gao@gmail.com>
2017-04-10 10:44:33 +0800

    wip: a DAGBuilder

--- engine/builder.go
+++ engine/builder.go
@@ -4,8 +4,31 @@ import (
 	conf "github.com/funkygao/jsconf"
 )
 
-// DAGBuilder is the builder that helps to build a pipeline DAG.
+/*
+DAGBuilder is the builder that helps to build a pipeline DAG.
+
+Example:
+
+	package main
+
+	import (
+		"time"
+
+		"github.com/funkygao/dbus/engine"
+	)
+
+	func main() {
+		builder := engine.NewDAGBuilder()
+		builder.Input("in.mock", "MockInput").
+		    Filter("filter.mock", "MockFilter", "in.mock").
+			Output("out.mock", "MockOutput", "filter.mock").
+			    SetDuration("sleep", time.Second)
+
+		engine.New(nil).SubmitDAG(builder.CreateDAG()).ServeForever()
+	}
+*/
 type DAGBuilder struct {
+	*conf.Conf
 }
 
 // NewDAGBuilder creates a DAGBuilder.
@@ -17,11 +40,11 @@ func (b *DAGBuilder) Input(name, class string) *DAGBuilder {
 	return b
 }
 
-func (b *DAGBuilder) Filter(name, class string) *DAGBuilder {
+func (b *DAGBuilder) Filter(name, class string, matches ...string) *DAGBuilder {
 	return b
 }
 
-func (b *DAGBuilder) Output(name, class string) *DAGBuilder {
+func (b *DAGBuilder) Output(name, class string, matches ...string) *DAGBuilder {
 	return b
 }
 
@@ -29,6 +52,9 @@ func (b *DAGBuilder) Validate() error {
 	return nil
 }
 
-func (b *DAGBuilder) CreateDAG() *conf.Conf {
+func (b *DAGBuilder) CreateDAG() *DAGBuilder {
+	if err := b.Validate(); err != nil {
+		panic(err)
+	}
 	return nil
 }
