f3b160f7b81da365b75f6238e63df8da8f69857b
Funky Gao <funky.gao@gmail.com>
2017-04-11 10:54:11 +0800

    BUG FIX: after rebalance, myslave metrics hangs

--- pkg/myslave/metrics.go
+++ pkg/myslave/metrics.go
@@ -8,8 +8,9 @@ import (
 )
 
 type slaveMetrics struct {
-	Lag metrics.Gauge
+	tag string
 
+	Lag    metrics.Gauge
 	TPS    metrics.Meter
 	Events metrics.Meter
 }
@@ -17,8 +18,16 @@ type slaveMetrics struct {
 func newMetrics(name string) *slaveMetrics {
 	tag := telemetry.Tag(strings.Replace(name, ".", "_", -1), "", "v1")
 	return &slaveMetrics{
+		tag:    tag,
 		Lag:    metrics.NewRegisteredGauge(tag+"mysql.binlog.lag", metrics.DefaultRegistry),
 		TPS:    metrics.NewRegisteredMeter(tag+"mysql.binlog.tps", metrics.DefaultRegistry),
 		Events: metrics.NewRegisteredMeter(tag+"mysql.binlog.evt", metrics.DefaultRegistry),
 	}
+}
+
+func (m *slaveMetrics) Close() {
+	// TODO flush metrics
+	metrics.Unregister(m.tag + "mysql.binlog.lag")
+	metrics.Unregister(m.tag + "mysql.binlog.tps")
+	metrics.Unregister(m.tag + "mysql.binlog.evt")
 }
