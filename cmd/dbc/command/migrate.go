8e5321ddcc085971b524738ffd37f2a634ed5561
Funky Gao <funky.gao@gmail.com>
2017-04-24 15:42:24 +0800

    dbc migrate

--- cmd/dbc/command/migrate.go
+++ cmd/dbc/command/migrate.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 
 	"github.com/funkygao/gocli"
+	"github.com/github/gh-ost/go/base"
+	"github.com/github/gh-ost/go/logic"
+	"github.com/outbrain/golib/log"
 )
 
 type Migrate struct {
@@ -14,31 +17,42 @@ type Migrate struct {
 }
 
 func (this *Migrate) Run(args []string) (exitCode int) {
+	ctx := base.GetMigrationContext()
 	cmdFlags := flag.NewFlagSet("migrate", flag.ContinueOnError)
+	cmdFlags.StringVar(&ctx.InspectorConnectionConfig.Key.Hostname, "host", "127.0.0.1", "")
+	cmdFlags.IntVar(&ctx.InspectorConnectionConfig.Key.Port, "port", 3306, "")
+	cmdFlags.StringVar(&ctx.DatabaseName, "db", "", "")
+	cmdFlags.StringVar(&ctx.OriginalTableName, "table", "", "")
+	cmdFlags.StringVar(&ctx.AlterStatement, "alter", "", "")
+	cmdFlags.StringVar(&ctx.CliUser, "user", "root", "")
+	cmdFlags.StringVar(&ctx.CliPassword, "pass", "", "")
 	cmdFlags.Usage = func() { this.Ui.Output(this.Help()) }
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
 	}
 
-	this.showWorkflow()
+	ctx.AllowedRunningOnMaster = true
+	ctx.OkToDropTable = true
+	ctx.InitiallyDropOldTable = true
+	ctx.InitiallyDropGhostTable = true
+	ctx.ServeSocketFile = "/tmp/dbc.migrate.sock"
+	ctx.ApplyCredentials()
+	log.EnableSyslogWriter("")
+	log.SetLevel(log.DEBUG)
+
+	log.Infof("starting")
+	migrator := logic.NewMigrator()
+	if err := migrator.Migrate(); err != nil {
+		migrator.ExecOnFailureHook()
+		log.Fatale(err)
+	}
+	log.Info("OK")
 
 	return
 }
 
-func (this *Migrate) showWorkflow() {
-	this.Ui.Outputf("option A:")
-	this.Ui.Outputf("    gh-ost --database a --table a --alter 'add bb int;' --user root --debug --allow-on-master --initially-drop-ghost-table --execute")
-	this.Ui.Output("")
-	this.Ui.Outputf("option B:")
-	this.Ui.Outputf("    mysqldump --host={host} --port={port} --user={user} --password={pass} --master-data --single-transaction --skip-lock-tables --compact --quick --skip-opt --no-create-info --skip-extended-insert --all-databases > d.sql")
-	this.Ui.Outputf("    locate 'CHANGE MASTER TO MASTER_LOG_FILE={name}, MASTER_LOG_POS={pos}' in d.sql")
-	this.Ui.Outputf("    import on target mysql instance: mysql < d.sql")
-	this.Ui.Outputf("    setup position for dbusd by {name}:{pos}, startup dbusd")
-
-}
-
 func (*Migrate) Synopsis() string {
-	return "Workflow of migrating a mysql database"
+	return "Asynchronous MySQL online schema migration tool"
 }
 
 func (this *Migrate) Help() string {
@@ -46,6 +60,21 @@ func (this *Migrate) Help() string {
 Usage: %s migrate [options]
 
     %s
+
+Options:
+
+    -user db username
+
+    -pass db password
+
+    -db dabase
+
+    -table table
+
+    -alter statement
+      e,g.
+      dbc migrate -db test -table x -alter 'add age int'
+
 
 `, this.Cmd, this.Synopsis())
 	return strings.TrimSpace(help)
