5358482c254efbe3ec49521f38d454e60b2766f6
Funky Gao <funky.gao@gmail.com>
2017-06-07 16:35:44 +0800

    BUG FIX: for recycleable chan, its free space is len of the chan

--- engine/api_v1.go
+++ engine/api_v1.go
@@ -50,20 +50,15 @@ func (e *Engine) handleQueuesV1(w http.ResponseWriter, r *http.Request, params m
 	rs := make(map[string]int)
 
 	globals := Globals()
-	rs["hub"] = len(e.router.hub)
 	rs["hub.free"] = globals.HubChanSize - rs["hub"]
+	rs["filter.free"] = len(e.filterRecycleChan)
 
 	for name, ch := range e.inputRecycleChans {
-		rs["input."+name] = len(ch)
-		rs["input."+name+".free"] = globals.InputRecyclePoolSize - rs["input."+name]
+		rs["input."+name+".free"] = len(ch)
 	}
 
-	rs["filter"] = len(e.filterRecycleChan)
-	rs["filter.free"] = globals.FilterRecyclePoolSize - rs["filter"]
-
 	for _, om := range e.router.outputMatchers {
-		rs["output."+om.runner.Name()] = len(om.InChan())
-		rs["output."+om.runner.Name()+".free"] = globals.PluginChanSize - rs["output."+om.runner.Name()]
+		rs["output."+om.runner.Name()+".free"] = globals.PluginChanSize - len(om.InChan())
 	}
 
 	return rs, nil
